ba9a33a57fda5543601fbea864d65d9b902dd9c9
Michael Ilyin <milyin@gmail.com>
2020-09-25 22:51:32 +0300

    game field in separate file

--- src/game_field.rs
+++ src/game_field.rs
@@ -1,25 +1,180 @@
 use std::collections::HashMap;
 
-use bindings::microsoft::graphics::canvas::text::CanvasTextLayout;
-use bindings::microsoft::graphics::canvas::ui::composition::CanvasComposition;
-use bindings::microsoft::graphics::canvas::CanvasDevice;
-use bindings::windows::ui::composition::Compositor;
-use bindings::windows::ui::composition::Visual;
+use bindings::{
+    microsoft::graphics::canvas::text::CanvasHorizontalAlignment,
+    microsoft::graphics::canvas::text::CanvasTextFormat,
+    microsoft::graphics::canvas::text::CanvasTextLayout,
+    microsoft::graphics::canvas::text::CanvasVerticalAlignment,
+    microsoft::graphics::canvas::ui::composition::CanvasComposition,
+    microsoft::graphics::canvas::CanvasDevice, windows::foundation::numerics::Vector2,
+    windows::foundation::numerics::Vector3, windows::foundation::Size,
+    windows::graphics::directx::DirectXAlphaMode, windows::graphics::directx::DirectXPixelFormat,
+    windows::ui::composition::CompositionBorderMode,
+    windows::ui::composition::CompositionGraphicsDevice,
+    windows::ui::composition::CompositionShape, windows::ui::composition::Compositor,
+    windows::ui::composition::ContainerVisual, windows::ui::composition::Visual,
+    windows::ui::Color, windows::ui::ColorHelper, windows::ui::Colors,
+};
+use model::field::{Field, Origin, Side};
+
+const TILE_SIZE: Vector2 = Vector2 { x: 512., y: 512. };
+const GAME_BOARD_MARGIN: Vector2 = Vector2 { x: 100.0, y: 100.0 };
+
+use crate::game_window::GameWindow;
 pub struct GameField {
     compositor: Compositor,
+    canvas_device: CanvasDevice,
+    composition_graphics_device: CompositionGraphicsDevice,
+    root: Visual,
+    field: Field,
+    game_board: ContainerVisual,
+    game_board_tiles: HashMap<(usize, usize), Visual>,
+    merged_tiles: Vec<Visual>,
+    tile_shapes: HashMap<u32, CompositionShape>,
     tile_text_layouts: HashMap<u32, CanvasTextLayout>,
 }
 
 impl GameField {
-    pub fn new(compositor: Compositor) -> Self {
-        let canvas_device = CanvasDevice::get_shared_device()?;
-        let composition_graphics_device =
-            CanvasComposition::create_composition_graphics_device(&compositor, &canvas_device)?;
-        Self {
+    pub fn new(game_window: &mut GameWindow) -> winrt::Result<Self> {
+        let compositor = game_window.compositor().clone();
+        let root = compositor.create_sprite_visual()?;
+        root.set_size(game_window.root().size()?)?;
+        root.set_offset(Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        })?;
+        //        root.set_brush(compositor.create_color_brush_with_color(Colors::red()?)?)?;
+        root.set_border_mode(CompositionBorderMode::Hard)?;
+        game_window.root().children()?.insert_at_top(&root)?;
+
+        let game_board = compositor.create_container_visual()?;
+        game_board.set_relative_offset_adjustment(Vector3 {
+            x: 0.5,
+            y: 0.5,
+            z: 0.0,
+        })?;
+        game_board.set_anchor_point(Vector2 { x: 0.5, y: 0.5 })?;
+        root.children()?.insert_at_top(&game_board)?;
+
+        //#[rustfmt::skip]
+        //let array =
+        //    Array2::from_shape_vec((4, 3), vec![2, 4, 4, 2, 2, 4, 0, 2, 2, 0, 0, 2]).unwrap();
+        //let mut field = Field::from_array(array);
+
+        let mut field = Field::new(4, 4);
+        field.append_tile();
+        field.append_tile();
+        field.fix_position();
+
+        let mut result = Self {
             compositor,
+            canvas_device: game_window.canvas_device().clone(),
+            composition_graphics_device: game_window.composition_graphics_device().clone(),
+            root: root.into(),
+            field,
+            game_board,
+            game_board_tiles: HashMap::new(),
+            merged_tiles: Vec::new(),
+            tile_shapes: HashMap::new(),
             tile_text_layouts: HashMap::new(),
+        };
+
+        result.init_game_board()?;
+
+        Ok(result)
+    }
+
+    pub fn undo(&mut self) -> winrt::Result<()> {
+        if self.field.can_undo() {
+            self.field.undo();
+            self.init_game_board()?;
+        }
+        Ok(())
+    }
+
+    pub fn swipe(&mut self, side: Side) -> winrt::Result<()> {
+        if self.field.can_swipe(side) {
+            self.field.swipe(side);
+            self.field.append_tile();
+            self.field.append_tile();
+            self.animate_game_board()?;
+        }
+        Ok(())
+    }
+
+    pub fn scale_game_board(&mut self) -> winrt::Result<()> {
+        let board_size = self.game_board.size()?;
+        let board_size = board_size + GAME_BOARD_MARGIN;
+
+        let window_size = self.root.size()?;
+
+        let window_ratio = window_size.x / window_size.y;
+        let board_ratio = board_size.x / board_size.y;
+
+        let scale_factor = if window_ratio > board_ratio {
+            window_size.y / board_size.y
+        } else {
+            window_size.x / board_size.x
+        };
+
+        self.game_board.set_scale(Vector3 {
+            x: scale_factor,
+            y: scale_factor,
+            z: 1.0,
+        })
+    }
+
+    pub fn get_tile_shape(&mut self, n: u32) -> winrt::Result<CompositionShape> {
+        if let Some(shape) = self.tile_shapes.get(&n) {
+            Ok(shape.clone())
+        } else {
+            let container_shape = self.compositor.create_container_shape()?;
+            let round_rect_geometry = self.compositor.create_rounded_rectangle_geometry()?;
+            round_rect_geometry.set_corner_radius(Vector2 {
+                x: TILE_SIZE.x / 20.,
+                y: TILE_SIZE.y / 20.,
+            })?;
+            let round_rect_size = TILE_SIZE / 1.1;
+            round_rect_geometry.set_size(&round_rect_size)?;
+            let brush = self
+                .compositor
+                .create_color_brush_with_color(Self::get_tile_color(n)?)?;
+            let round_rect = self
+                .compositor
+                .create_sprite_shape_with_geometry(round_rect_geometry)?;
+            round_rect.set_fill_brush(brush)?;
+            round_rect.set_offset((TILE_SIZE - round_rect_size) / 2.)?;
+            container_shape.shapes()?.append(round_rect)?;
+            let shape: CompositionShape = container_shape.into();
+            self.tile_shapes.insert(n, shape.clone());
+            Ok(shape)
         }
     }
+
+    pub fn get_tile_text_layout(&mut self, n: u32) -> winrt::Result<CanvasTextLayout> {
+        if let Some(text_layout) = self.tile_text_layouts.get(&n) {
+            Ok(text_layout.clone())
+        } else {
+            let text_string: String = n.to_string();
+            let text_format = CanvasTextFormat::new()?;
+            text_format.set_font_family("Arial")?;
+            text_format.set_font_size(Self::get_tile_font_size(n))?;
+
+            let text_layout = CanvasTextLayout::create(
+                &self.canvas_device,
+                text_string,
+                text_format,
+                TILE_SIZE.x,
+                TILE_SIZE.y,
+            )?;
+            text_layout.set_vertical_alignment(CanvasVerticalAlignment::Center)?;
+            text_layout.set_horizontal_alignment(CanvasHorizontalAlignment::Center)?;
+            self.tile_text_layouts.insert(n, text_layout.clone());
+            Ok(text_layout)
+        }
+    }
+
     fn create_tile_visual(&mut self, x: usize, y: usize, n: u32) -> winrt::Result<Visual> {
         let surface = self.composition_graphics_device.create_drawing_surface(
             Size {
@@ -64,5 +219,196 @@ impl GameField {
         let visual: Visual = tile_visual.into();
         Self::animated_appear_tile(&visual)?;
         Ok(visual)
+    }
+
+    fn hold_tile_visual(&mut self, x: usize, y: usize) -> winrt::Result<Visual> {
+        Ok(self
+            .game_board_tiles
+            .remove(&(x, y))
+            .expect("hold emplty tile"))
+    }
+
+    fn animated_move_tile(
+        visual: &Visual,
+        from_x: usize,
+        from_y: usize,
+        x: usize,
+        y: usize,
+    ) -> winrt::Result<()> {
+        let compositor = visual.compositor()?;
+        let animation = compositor.create_vector3_key_frame_animation()?;
+        let animate_from = Vector3 {
+            x: TILE_SIZE.x * from_x as f32,
+            y: TILE_SIZE.y * from_y as f32,
+            z: 0.,
+        };
+        let animate_to = Vector3 {
+            x: TILE_SIZE.x * x as f32,
+            y: TILE_SIZE.y * y as f32,
+            z: 0.,
+        };
+        animation.insert_key_frame(0.0, animate_from)?;
+        animation.insert_key_frame(1.0, animate_to)?;
+        visual.start_animation("Offset", animation)?;
+        Ok(())
+    }
+
+    fn animated_appear_tile(visual: &Visual) -> winrt::Result<()> {
+        let compositor = visual.compositor()?;
+
+        let animation = compositor.create_vector3_key_frame_animation()?;
+        let animate_from = Vector3 {
+            x: 0.,
+            y: 0.,
+            z: 0.,
+        };
+        let animate_to = Vector3 {
+            x: 1.,
+            y: 1.,
+            z: 0.,
+        };
+        animation.insert_key_frame(0.0, animate_from)?;
+        animation.insert_key_frame(1.0, animate_to)?;
+        let size = visual.size()?;
+        visual.set_center_point(Vector3 {
+            x: size.x / 2.,
+            y: size.y / 2.,
+            z: 0.,
+        })?;
+        visual.start_animation("Scale", animation)?;
+        Ok(())
+    }
+
+    fn move_tile_visual(
+        &mut self,
+        from_x: usize,
+        from_y: usize,
+        x: usize,
+        y: usize,
+    ) -> winrt::Result<Visual> {
+        let visual = self
+            .game_board_tiles
+            .remove(&(from_x, from_y))
+            .expect("move emplty tile");
+        Self::animated_move_tile(&visual, from_x, from_y, x, y)?;
+        Ok(visual)
+    }
+
+    fn merge_tile_visuals(
+        &mut self,
+        from_x1: usize,
+        from_y1: usize,
+        from_x2: usize,
+        from_y2: usize,
+        x: usize,
+        y: usize,
+        n: u32,
+    ) -> winrt::Result<Visual> {
+        let visual1 = self
+            .game_board_tiles
+            .remove(&(from_x1, from_y1))
+            .expect("merge emplty tile");
+        let visual2 = self
+            .game_board_tiles
+            .remove(&(from_x2, from_y2))
+            .expect("merge emplty tile");
+        Self::animated_move_tile(&visual1, from_x1, from_y1, x, y)?;
+        Self::animated_move_tile(&visual2, from_x2, from_y2, x, y)?;
+        self.merged_tiles.push(visual1);
+        self.merged_tiles.push(visual2);
+        self.create_tile_visual(x, y, n)
+    }
+
+    fn init_game_board(&mut self) -> winrt::Result<()> {
+        self.game_board.set_size(Vector2 {
+            x: self.field.width() as f32 * TILE_SIZE.x,
+            y: self.field.height() as f32 * TILE_SIZE.y,
+        })?;
+        self.remove_merged_tiles()?;
+        self.game_board.children()?.remove_all()?;
+        //self.draw_game_board_border()?;
+        self.game_board_tiles.clear();
+        for x in 0..self.field.width() {
+            for y in 0..self.field.height() {
+                if let Some(tile) = self.field.get(x, y) {
+                    let n = tile.get_n();
+                    let tile = self.create_tile_visual(x, y, n)?;
+                    self.game_board_tiles.insert((x, y), tile);
+                }
+            }
+        }
+        self.scale_game_board()
+    }
+
+    fn remove_merged_tiles(&mut self) -> winrt::Result<()> {
+        while let Some(tile) = self.merged_tiles.pop() {
+            self.game_board.children()?.remove(tile)?;
+        }
+        Ok(())
+    }
+
+    fn animate_game_board(&mut self) -> winrt::Result<()> {
+        self.remove_merged_tiles()?;
+        let mut new_board_tiles = HashMap::new();
+        for x in 0..self.field.width() {
+            for y in 0..self.field.height() {
+                if let Some(tile) = self.field.get(x, y) {
+                    let n = tile.get_n();
+                    match tile.get_origin() {
+                        Origin::Appear => {
+                            new_board_tiles.insert((x, y), self.create_tile_visual(x, y, n)?);
+                        }
+                        Origin::Hold { .. } => {
+                            new_board_tiles.insert((x, y), self.hold_tile_visual(x, y)?);
+                        }
+                        Origin::Moved(from_x, from_y) => {
+                            new_board_tiles
+                                .insert((x, y), self.move_tile_visual(from_x, from_y, x, y)?);
+                        }
+                        Origin::Merged((from_x1, from_y1), (from_x2, from_y2)) => {
+                            new_board_tiles.insert(
+                                (x, y),
+                                self.merge_tile_visuals(
+                                    from_x1, from_y1, from_x2, from_y2, x, y, n,
+                                )?,
+                            );
+                        }
+                    }
+                }
+            }
+        }
+        assert!(self.game_board_tiles.is_empty());
+        self.game_board_tiles = new_board_tiles;
+        Ok(())
+    }
+
+    fn get_tile_color(n: u32) -> winrt::Result<Color> {
+        match n {
+            1 => Colors::gray(),
+            2 => ColorHelper::from_argb(255, 238, 228, 218),
+            4 => ColorHelper::from_argb(255, 237, 224, 200),
+            8 => ColorHelper::from_argb(255, 242, 177, 121),
+            16 => ColorHelper::from_argb(255, 242, 177, 121),
+            32 => ColorHelper::from_argb(255, 246, 124, 95),
+            64 => ColorHelper::from_argb(255, 246, 124, 95),
+            128 => ColorHelper::from_argb(255, 237, 207, 114),
+            256 => ColorHelper::from_argb(255, 237, 207, 97),
+            512 => ColorHelper::from_argb(255, 237, 200, 80),
+            1024 => ColorHelper::from_argb(255, 237, 197, 63),
+            2048 => ColorHelper::from_argb(255, 237, 194, 46),
+            _ => ColorHelper::from_argb(255, 60, 58, 60),
+        }
+    }
+
+    fn get_tile_font_size(n: u32) -> f32 {
+        if n < 1000 {
+            256.
+        } else if n < 10000 {
+            180.
+        } else if n < 100000 {
+            150.
+        } else {
+            120.
+        }
     }
 }
