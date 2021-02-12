6105c0f6140627e0f63f5aebbe50f9e7aa7d2091
Michael Ilyin <milyin@gmail.com>
2021-02-12 18:59:18 +0300

    switch to widows-rs 3.1

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,11 +1,11 @@
 fn main() {
     windows::build!(
-        windows::foundation::numerics::{Vector2, Vector3}
-        windows::foundation::TimeSpan
-        windows::graphics::SizeInt32
-        windows::system::DispatcherQueueController
-        windows::win32::system_services::CreateDispatcherQueueController
-        windows::win32::system_services::DispatcherQueueOptions
+        windows::foundation::numerics::{Vector2, Vector3},
+        windows::foundation::TimeSpan,
+        windows::graphics::SizeInt32,
+        windows::system::DispatcherQueueController,
+        windows::win32::system_services::CreateDispatcherQueueController,
+        windows::win32::system_services::DispatcherQueueOptions,
         windows::ui::composition::{
             AnimationIterationBehavior,
             CompositionBatchTypes,
@@ -17,12 +17,12 @@ fn main() {
             Compositor,
             ContainerVisual,
             SpriteVisual,
-        }
-        windows::ui::composition::desktop::DesktopWindowTarget
-        windows::ui::{Colors, ColorHelper}
-        windows::win32::winrt::{ICompositorDesktopInterop, RoInitialize}
-        microsoft::graphics::canvas::{CanvasDevice}
-        microsoft::graphics::canvas::text::*
-        microsoft::graphics::canvas::ui::composition::*
+        },
+        windows::ui::composition::desktop::DesktopWindowTarget,
+        windows::ui::{Colors, ColorHelper},
+        windows::win32::winrt::{ICompositorDesktopInterop, RoInitialize},
+        microsoft::graphics::canvas::{CanvasDevice},
+        microsoft::graphics::canvas::text::*,
+        microsoft::graphics::canvas::ui::composition::*,
     );
 }
